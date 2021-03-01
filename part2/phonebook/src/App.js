00fef4c99c815363456caed4a95913a5888dd9de
ngntrung <nguyentrungux@gmail.com>
2021-03-01 20:45:49 +0700

    submit phonebook exercise

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => {
   return (
@@ -23,23 +24,28 @@ const PersonForm = (props) => {
       </div>
     </form>
   )
-
 }
+
 const Persons = ({results}) => {
   return (
     results.map(person => <p key={person.name}>{person.name} {person.number}</p>)
   )
 }
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchInput, setSearchInput ] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPersonEvent = (event) => {
     event.preventDefault()

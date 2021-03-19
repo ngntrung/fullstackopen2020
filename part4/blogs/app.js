2e4b53e0d6e02fb43010efd7d9d2893c82ec4b7b
Trung Nguyen <nguyentrungux@gmail.com>
2021-03-19 15:57:17 +0700

    update blogs exercises

---
+++ part4/blogs/app.js
@@ -0,0 +1,23 @@
+
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blog_controller')
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
+module.exports = app
